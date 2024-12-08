f61419476a3cdedd47ffbbad89c0ae9417794de1
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-12-08 17:56:30 +0200

    add products table and touch the blade

---
+++ exmaple-app/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- here is just tells about the var -->
+@isset($name)
+the name is :  <b>{{$name}}</b>
+@endisset
+    
+</body>
+</html>
--- exmaple-app/routes/web.php
+++ exmaple-app/routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// home directory here
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/products/{product_name}', function ($product_name) {
+    return redirect()->route('hello') ;
+})->name('hello');
+
+Route::get('/myView', function () {
+    return view('index' , ['name' => 'serag']);
+});
+
+
+// we can make a not found route for ourselves
+Route::fallback(function () {
+    return view('');
 });
